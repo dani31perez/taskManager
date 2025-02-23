6f00bbbc1a3bf4ce7cf8338bca03668196f3e3ae
dani31perez <perezbarredad@gmail.com>
2025-02-23 11:24:09 -0600

    Implemented handleSubmit function to add new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import FormTasks from "./components/FormTasks";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
 
   return (
     <>
+      <div className="container-fluid container-global">
+        <h1 className="text-secondary-emphasis">Task Manager</h1>
+        <FormTasks tasks={tasks} setTasks={setTasks}/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FormTasks.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Form, FloatingLabel, Button } from "react-bootstrap";
+
+const FormTasks = ({ tasks, setTasks }) => {
+  const [task, setTask] = useState("");
+  const [category, setCategory] = useState("Choose");
+  const [validate, setValidate] = useState(false);
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    e.preventDefault();
+    if (form.checkValidity() === false){
+        setValidate(true);
+    } else {
+      setTasks([...tasks, { name: task, category: category }]);
+      setTask("");
+      setCategory("");
+      setValidate(false);
+    }
+
+  };
+
+  return (
+    <Form
+      noValidate
+      validated={validate}
+      onSubmit={handleSubmit}
+      className="text-start"
+    >
+      <FloatingLabel
+        controlId="floatingTask"
+        label="Add a new task"
+        className="my-4"
+      >
+        <Form.Control
+          required
+          type="text"
+          placeholder="Add a new task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <Form.Control.Feedback type="invalid">
+          Please enter a task to add
+        </Form.Control.Feedback>
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingSelect" label="Category">
+        <Form.Select
+          aria-label="Category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+        >
+          <option value="">Choose a category</option>
+          <option value="Personal">Personal</option>
+          <option value="Work">Work</option>
+          <option value="Study">Study</option>
+        </Form.Select>
+        <Form.Control.Feedback type="invalid">
+          Please choose a category
+        </Form.Control.Feedback>
+      </FloatingLabel>
+      <div className="d-grid mt-4">
+        <Button variant="secondary" size="lg" type="submit">
+          Add task
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default FormTasks;
